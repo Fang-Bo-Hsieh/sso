c2950ec0432bc458eafdc2e28688495003e76111
baobeilai <baobeilai@gmail.com>
2017-12-14 09:08:14 +0800

    Edit encode function
    1. 將加密方式做修正

--- src/Broker.php
+++ src/Broker.php
@@ -371,18 +371,19 @@ class Broker
      * @return string
      */
     public function encode($string,$key) {
-        $hash = '';
-        $key = sha1($key);
-        $strLen = strlen($string);
-        $keyLen = strlen($key);
-        $j = 0;
-        for ($i = 0; $i < $strLen; $i++) {
-            $ordStr = ord(substr($string,$i,1));
-            if ($j == $keyLen) { $j = 0; }
-            $ordKey = ord(substr($key,$j,1));
-            $j++;
-            $hash .= strrev(base_convert(dechex($ordStr + $ordKey),16,36));
-        }
+//        $hash = '';
+        $hash = sha1($key.$string);
+//        $strLen = strlen($string);
+//        $keyLen = strlen($key);
+//        $j = 0;
+//        for ($i = 0; $i < $strLen; $i++) {
+//            $ordStr = ord(substr($string,$i,1));
+//            if ($j == $keyLen) { $j = 0; }
+//            $ordKey = ord(substr($key,$j,1));
+//            $j++;
+//            $hash .= strrev(base_convert(dechex($ordStr + $ordKey),16,36));
+//        }
+
         return $hash;
     }
 
